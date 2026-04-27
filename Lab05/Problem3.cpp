ce8770faa41dd62e7eee5414e8b913d729b50ca7
BruceHuangQQ <brucewonghh@outlook.com>
2026-04-27 12:26:39 +1000

    Lab05

---
+++ Lab05/Problem3.cpp
@@ -0,0 +1,31 @@
+
+#include "DataWrapper.h"
+
+#include <iostream>
+#include <cassert>
+
+void Problem3( const DataWrapper& aWrapper )
+{
+    std::cout << "Sorting the data: " << std::endl;
+
+    aWrapper.apply( [&aWrapper](size_t i){
+        
+            bool lFound = false;
+            size_t j = 0;
+        
+            for ( ; j < aWrapper.size(); j++ )
+            {
+                if ( aWrapper[j].key() == i )
+                {
+                    lFound = true;
+                    break;
+                }
+            }
+        
+            assert( lFound );
+        
+            std::cout << static_cast<char>(aWrapper[j]);
+        } );
+
+    std::cout << std::endl;
+}
